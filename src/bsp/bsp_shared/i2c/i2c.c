bc488c4ec4d6688413e2c54580b00427ab61c95c
Harry Rostovtsev <harry_rostovtsev@datacard.com>
2014-07-16 15:57:15 -0500

    Got DMA read to work.  Need to make sure we close the I2C connection correctly still.

--- src/bsp/bsp_shared/i2c/i2c.c
+++ src/bsp/bsp_shared/i2c/i2c.c
@@ -179,10 +179,10 @@ void I2C_BusInit( I2C_Bus_t iBus )
 //   );
 
    /* Initialize the IRQ and priorities for I2C DMA */
-//   NVIC_Config(
-//         s_I2C_Bus[iBus].i2c_dma_rx_irq_num,
-//         s_I2C_Bus[iBus].i2c_dma_rx_irq_prio
-//   );
+   NVIC_Config(
+         s_I2C_Bus[iBus].i2c_dma_rx_irq_num,
+         s_I2C_Bus[iBus].i2c_dma_rx_irq_prio
+   );
 //
 //   NVIC_Config(
 //         s_I2C_Bus[iBus].i2c_dma_tx_irq_num,
@@ -214,54 +214,54 @@ void I2C_DMARead( I2C_Bus_t iBus, uint16_t wReadAddr, uint16_t wReadLen )
    /* Check inputs */
    assert_param( IS_I2C_BUS( iBus ) );
 
-//   /* Set the structure buffer management for how many bytes to expect and how
-//    * many are currently there (none). */
-//   s_I2C_Bus[iBus].nBytesExpected = wReadLen;
-//   s_I2C_Bus[iBus].nBytesCurrent  = 0;
-//   s_I2C_Bus[iBus].nRxindex       = 0;
-//
-//   /* Clear out the DMA settings */
-//   DMA_DeInit( s_I2C_Bus[iBus].i2c_dma_rx_stream );
-//
-//   /* Set up a new DMA structure for reading */
-//   DMA_InitStructure.DMA_Channel             = s_I2C_Bus[iBus].i2c_dma_channel;
-//   DMA_InitStructure.DMA_PeripheralBaseAddr  = s_I2C_Bus[iBus].i2c_dma_dr_addr;
-//   DMA_InitStructure.DMA_DIR                 = DMA_DIR_PeripheralToMemory;
-//   DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;
-//   DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Enable;
-//   DMA_InitStructure.DMA_PeripheralDataSize  = DMA_PeripheralDataSize_Byte;
-//   DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_Byte;
-//   DMA_InitStructure.DMA_Mode                = DMA_Mode_Normal;
-//   DMA_InitStructure.DMA_Priority            = DMA_Priority_VeryHigh;
-//   DMA_InitStructure.DMA_FIFOMode            = DMA_FIFOMode_Enable;
-//   DMA_InitStructure.DMA_FIFOThreshold       = DMA_FIFOThreshold_Full;
-//   DMA_InitStructure.DMA_MemoryBurst         = DMA_MemoryBurst_Single;
-//   DMA_InitStructure.DMA_PeripheralBurst     = DMA_PeripheralBurst_Single;
-//   DMA_InitStructure.DMA_Memory0BaseAddr     = s_I2C_Bus[iBus].pRxBuffer;
-//   DMA_InitStructure.DMA_BufferSize          = s_I2C_Bus[iBus].nBytesExpected;
-//
-//   /* Initialize the DMA with the filled in structure */
-//   DMA_Init( s_I2C_Bus[iBus].i2c_dma_rx_stream, &DMA_InitStructure );
-//
-//   /* Enable the TransferComplete interrupt in the DMA */
-//   DMA_ITConfig( s_I2C_Bus[iBus].i2c_dma_rx_stream, DMA_IT_TC, ENABLE );
-//
-//   /* Clear any pending flags on RX Stream */
-//   DMA_ClearFlag(
-//         s_I2C_Bus[iBus].i2c_dma_rx_stream,
-//         DMA_FLAG_TCIF0 |
-//         DMA_FLAG_FEIF0 |
-//         DMA_FLAG_DMEIF0 |
-//         DMA_FLAG_TEIF0 |
-//         DMA_FLAG_HTIF0
-//   );
-//
-//   /* I2Cx DMA Enable */
-//   I2C_DMACmd( s_I2C_Bus[iBus].i2c_bus, ENABLE );
-//
-//   /* Finally, activate DMA */
-//   DMA_Cmd( s_I2C_Bus[iBus].i2c_dma_rx_stream, ENABLE );
-   DBG_printf("Sent fake DMA read\n");
+   /* Set the structure buffer management for how many bytes to expect and how
+    * many are currently there (none). */
+   s_I2C_Bus[iBus].nBytesExpected = wReadLen;
+   s_I2C_Bus[iBus].nBytesCurrent  = 0;
+   s_I2C_Bus[iBus].nRxindex       = 0;
+
+   /* Clear out the DMA settings */
+   DMA_DeInit( s_I2C_Bus[iBus].i2c_dma_rx_stream );
+
+   /* Set up a new DMA structure for reading */
+   DMA_InitStructure.DMA_Channel             = s_I2C_Bus[iBus].i2c_dma_channel;
+   DMA_InitStructure.DMA_PeripheralBaseAddr  = s_I2C_Bus[iBus].i2c_dma_dr_addr;
+   DMA_InitStructure.DMA_DIR                 = DMA_DIR_PeripheralToMemory;
+   DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;
+   DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Enable;
+   DMA_InitStructure.DMA_PeripheralDataSize  = DMA_PeripheralDataSize_Byte;
+   DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_Byte;
+   DMA_InitStructure.DMA_Mode                = DMA_Mode_Normal;
+   DMA_InitStructure.DMA_Priority            = DMA_Priority_VeryHigh;
+   DMA_InitStructure.DMA_FIFOMode            = DMA_FIFOMode_Enable;
+   DMA_InitStructure.DMA_FIFOThreshold       = DMA_FIFOThreshold_Full;
+   DMA_InitStructure.DMA_MemoryBurst         = DMA_MemoryBurst_Single;
+   DMA_InitStructure.DMA_PeripheralBurst     = DMA_PeripheralBurst_Single;
+   DMA_InitStructure.DMA_Memory0BaseAddr     = s_I2C_Bus[iBus].pRxBuffer;
+   DMA_InitStructure.DMA_BufferSize          = s_I2C_Bus[iBus].nBytesExpected;
+
+   /* Initialize the DMA with the filled in structure */
+   DMA_Init( s_I2C_Bus[iBus].i2c_dma_rx_stream, &DMA_InitStructure );
+
+   /* Enable the TransferComplete interrupt in the DMA */
+   DMA_ITConfig( s_I2C_Bus[iBus].i2c_dma_rx_stream, DMA_IT_TC, ENABLE );
+
+   /* Clear any pending flags on RX Stream */
+   DMA_ClearFlag(
+         s_I2C_Bus[iBus].i2c_dma_rx_stream,
+         DMA_FLAG_TCIF0 |
+         DMA_FLAG_FEIF0 |
+         DMA_FLAG_DMEIF0 |
+         DMA_FLAG_TEIF0 |
+         DMA_FLAG_HTIF0
+   );
+
+   /* I2Cx DMA Enable */
+   I2C_DMACmd( s_I2C_Bus[iBus].i2c_bus, ENABLE );
+
+   /* Finally, activate DMA */
+   DMA_Cmd( s_I2C_Bus[iBus].i2c_dma_rx_stream, ENABLE );
+   DBG_printf("Sent DMA read\n");
 }
 
 /**
@@ -273,9 +273,9 @@ void I2C_DMARead( I2C_Bus_t iBus, uint16_t wReadAddr, uint16_t wReadLen )
 void DMA1_Stream0_IRQHandler( void )
 {
    QK_ISR_ENTRY();                         /* inform QK about entering an ISR */
-
+   isr_dbg_slow_printf("DMA1_Stream0_IRQHandler\n");
    /* Test on DMA Stream Transfer Complete interrupt */
-   if ( RESET != DMA_GetITStatus(DMA1_Stream0, DMA_IT_TCIF4) ) {
+   if ( RESET != DMA_GetITStatus(DMA1_Stream0, DMA_IT_TCIF0) ) {
       /* Disable DMA so it doesn't keep outputting the buffer. */
       DMA_Cmd( DMA1_Stream0, DISABLE );
 
@@ -291,7 +291,7 @@ void DMA1_Stream0_IRQHandler( void )
       QF_PUBLISH( (QEvent *)i2cDataEvt, AO_SerialMgr );
 
       /* Clear DMA Stream Transfer Complete interrupt pending bit */
-      DMA_ClearITPendingBit( DMA1_Stream0, DMA_IT_TCIF4 );
+      DMA_ClearITPendingBit( DMA1_Stream0, DMA_IT_TCIF0 );
    }
 
    QK_ISR_EXIT();                           /* inform QK about exiting an ISR */
